c6e5b532ad0aea87bbbdd19937584bf26102df51
MAC31 <mac31@MacBook-Pro-de-MAC31.local>
2022-10-14 11:02:51 -0500

    feat - Added read a song by id

--- src/components/song/controller.js
+++ src/components/song/controller.js
@@ -34,3 +34,20 @@ export const create = async (req, res) => {
    });
  }
 };
+
+export const findOne = async (req, res) => {
+    try{
+        const { id } = req.params
+        const songs = await Song.findOne({_id:id});
+        res.json({
+            ok: true,
+            data: songs,
+        });
+    }
+    catch (error) {
+        res.json({
+          ok: false,
+          data: error.message,
+        });
+      }
+}
