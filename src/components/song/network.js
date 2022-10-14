4ffacb5baa6a9895b027747b8357d195d57fa1e1
MAC31 <mac31@MacBook-Pro-de-MAC31.local>
2022-10-14 10:46:32 -0500

    feat: Added models song and playlist

--- src/components/song/network.js
+++ src/components/song/network.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import * as Controller from "./controller";
+import authentication from "../../middlewares/authentication"
 
 const songRouter = Router();
 
-songRouter.route("/").get(Controller.findAll);
-songRouter.route("/").post(Controller.create);
+songRouter.route("/").get(authentication, Controller.findAll);
+songRouter.route("/").post(authentication, Controller.create);
 
 export default songRouter;
