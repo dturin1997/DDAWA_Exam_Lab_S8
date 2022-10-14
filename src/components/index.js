113263ef71f6c87ccb1889d349e99317c05dd926
MAC44 <mac44@MacBook-Pro-de-MAC44.local>
2022-10-14 11:17:10 -0500

    feat - added playlist's read and create

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,6 @@ export { default as SongRouter } from "./song/network";
 
 export { default as authRouter } from "./auth/network";
 
+export { default as playListRouter } from "./playList/network";
+
+
--- src/models/playList.js
+++ src/models/playList.js
@@ -3,22 +3,10 @@ import mongoose from "mongoose";
 
 const playListSchema = mongoose.Schema({
     name: { type: String, required: true },
-    userId: { type: String, required: true },
-    songs: { type: mongoose.Schema.Types.ObjectId, ref: 'Song' },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true },
+    songs: { type: mongoose.Schema.Types.ObjectId, ref: 'Song', required:true },
 });
 
 export default mongoose.model("PlayList", playListSchema);
-/*
-const storySchema = mongoose.Schema({
-    caption: { type: String, required: true },
-    username: { type: String, required: true },
-    userId: { type: String, required: true },
-    //userId: { type: String },
-    image: { type: String, required: true },
-    tags: String,
-    //likes: { type: Number, default: 0 },
-    likes: { type: [String], default: [] },
-    postDate: { type: Date, default: new Date() },
-});
-*/
+
 
